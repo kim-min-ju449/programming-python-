39fc47070e590ca539f8775885c6688dfd825945
김민주 <w2023@e-mirim.hs.kr>
2021-09-24 09:42:42 +0900

    예외처리커밋

---
+++ main.py
@@ -0,0 +1,17 @@
+def solution(scores):
+    a=sum(scores)
+def 최소공배수(a,b):
+    if a *1%b ==0:
+        return a *1
+    elif a *2%b ==0:
+        return a *2
+    elif a *3%b ==0:
+        return a *3
+    elif a *4%b ==0:
+        return a *4
+    elif a *5%b ==0:
+        return a *5
+if __name__ =="__main__":
+    a= 4
+    b=6
+    최소공배수(4,6)
